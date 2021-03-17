a1945d59bce1d2735c90b6f8e2a851ae7df6b264
aaren <popstarfreas2@gmail.com>
2021-03-17 16:46:52 +0000

    build(plugin): Don't use linux commands

--- build.plugin.js
+++ build.plugin.js
@@ -2,16 +2,30 @@ var child_process = require("child_process")
 var fs = require("fs")
 var path = require("path")
 
+function copyDir(src, dest) {
+    fs.mkdirSync(dest, { recursive: true });
+    let entries = fs.readdirSync(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ?
+            copyDir(srcPath, destPath) :
+            fs.copyFileSync(srcPath, destPath);
+    }
+}
+
 console.log("Build Step 1 of 5")
-child_process.execSync("rm package.json")
-child_process.execSync("rm package-lock.json")
-child_process.execSync("mkdir node_modules")
+fs.unlinkSync("package.json")
+fs.unlinkSync("package-lock.json")
+fs.mkdirSync("node_modules")
 child_process.execSync("npm i --no-package-lock --no-save rollup@2.41.0")
 child_process.execSync("npm i --no-package-lock --no-save @rollup/plugin-commonjs@17.1.0")
 child_process.execSync("npm i --no-package-lock --no-save @rollup/plugin-json@4.1.0")
 child_process.execSync("npm i --no-package-lock --no-save @rollup/plugin-node-resolve@11.2.0")
 console.log("Build Step 2 of 5")
-child_process.execSync("cp -r ../../pluginreference ./node_modules/terrariaserver-lite")
+copyDir("../../pluginreference", "./node_modules/terrariaserver-lite")
 console.log("Build Step 3 of 5")
 child_process.execSync("../../node_modules/bs-platform/bsb -make-world")
 console.log("Build Step 4 of 5")
