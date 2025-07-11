bc11ad0186f804867147f22329250f0e3d8fdbeb
popstarfreas <popstarfreas2@gmail.com>
2025-07-11 10:25:12 +0100

    chore: Update to pnpm

--- build.plugin.js
+++ build.plugin.js
@@ -3,26 +3,26 @@ var fs = require("fs")
 var path = require("path")
 
 function copyDir(src, dest) {
-    fs.mkdirSync(dest, { recursive: true });
-    let entries = fs.readdirSync(src, { withFileTypes: true });
+  fs.mkdirSync(dest, { recursive: true });
+  let entries = fs.readdirSync(src, { withFileTypes: true });
 
-    for (let entry of entries) {
-        let srcPath = path.join(src, entry.name);
-        let destPath = path.join(dest, entry.name);
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
 
-        entry.isDirectory() ?
-            copyDir(srcPath, destPath) :
-            fs.copyFileSync(srcPath, destPath);
-    }
+    entry.isDirectory() ?
+      copyDir(srcPath, destPath) :
+      fs.copyFileSync(srcPath, destPath);
+  }
 }
 
 console.log("Build Step 1 of 5")
-child_process.execSync("yarn remove terrariaserver-lite", { stdio: "inherit" })
+child_process.execSync("pnpm remove terrariaserver-lite", { stdio: "inherit" })
 console.log("Build Step 2 of 5")
-child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("pnpm add ../../pluginreference", { stdio: "inherit" })
 console.log("Build Step 3 of 5")
-child_process.execSync("npm run build:core", { stdio: "inherit" })
+child_process.execSync("pnpm run build:core", { stdio: "inherit" })
 console.log("Build Step 4 of 5")
-child_process.execSync("npm run build:packed", { stdio: "inherit" })
+child_process.execSync("pnpm run build:packed", { stdio: "inherit" })
 console.log("Build Step 5 of 5")
 fs.renameSync("./output", "./plugin")
